93cd35e3d625399cf06be013afdabec304c0598d
brannan-abigail <abigailbrannan1@gmail.com>
2021-08-16 15:58:08 -0500

    Modified layout (more modification needed)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <div>
-      <NavBar/>
+      <header className = "App-header">
+        <h1>Abby Brannan</h1>
+      </header>
+
+      <body><NavBar/></body>
+
+      <footer className = "App-footer">
+        <p>  
+          Connect With me: <a href="https://www.linkedin.com/in/abigail-brannan/"> https://www.linkedin.com/in/abigail-brannan/ </a>  |
+          Email me: <a href="abigailbrannan1@gmail.com"> abigailbrannan1@gmail.com </a>  |   
+          Call me: <a href="281-979-5003"> 281-979-5003 </a>
+        </p>
+      </footer>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,18 @@ import Headshot from "../resources/AbbyBrannan_LavGrad2021_Headshot.jpg";
 
 export default function Home() {
   return (
-    <div className = "home">
-        <h1>Abby Brannan</h1>
-        <p>
-            I'm an aspiring Software Engineer. I find solving technical 
-            problems satisfying, much like finishing a puzzle; especially as the field 
-            challenges one to continuously learn new languages, philosophies and techniques, 
-            which is quite exciting! Technology is a powerful tool for making positive change, 
-            and it's invigorating to be a part of it! 
-        </p>
+    <div className = "component"> 
+      <h2>Howdy!</h2>
 
-        <img src={Headshot}  alt="Abby Profile Pic" height={250} width={200}/> 
+      <p>
+        I'm an aspiring Software Engineer. I find solving technical problems satisfying, 
+        much like finishing a puzzle. Especially as the field challenges one to continuously 
+        learn new languages, philosophies and techniques, which is quite exciting! 
+        Technology is a powerful tool for making positive change, and it's invigorating to 
+        be a part of it.
+      </p>
+
+      <img src={Headshot}  alt="Abby Profile Pic" height={325} width={275}/> 
     </div>
   );
 }
